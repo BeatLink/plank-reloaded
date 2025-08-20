9955fe8d981a684dcef8646ef58ba22555635e9b
BeatLink <github@beatlink.simplelogin.com>
2025-08-20 18:48:54 -0500

    Update flake for multiplatform

--- flake.nix
+++ flake.nix
@@ -1,59 +1,66 @@
 {
-  description = "Plank Reloaded";
+    description = "Plank Reloaded";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, ... }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
-  in
-  {
-    packages.x86_64-linux.plank-reloaded = pkgs.stdenv.mkDerivation rec {
-      pname = "plank-reloaded";
-      version = "latest";
 
-      src = ./.;
+    outputs = { self, nixpkgs, ... }: let
+        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+        forAllSystems = arch: nixpkgs.lib.genAttrs systems (system: arch system);
+    in {
+        packages = forAllSystems (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in {
+                plank-reloaded = pkgs.stdenv.mkDerivation rec {
 
-      nativeBuildInputs = [
-        pkgs.meson
-        pkgs.ninja
-        pkgs.pkg-config
-        pkgs.vala
-        pkgs.glib
-        pkgs.bamf
-        pkgs.wrapGAppsHook  # Added for GSettings support
-      ];
+                    pname = "plank-reloaded";
 
-      buildInputs = [
-        pkgs.gnome-settings-daemon
-        pkgs.dconf
-        pkgs.glib
-        pkgs.git
-        pkgs.gtk3
-        pkgs.gnome-menus
-        pkgs.libgee
-        pkgs.libwnck
-        pkgs.pango
-        pkgs.desktop-file-utils
-      ];
+                    version = "latest";
 
-      # Compile schemas in post-install phase
-      postInstall = ''
-        glib-compile-schemas $out/share/glib-2.0/schemas
-      '';
+                    src = ./.;
 
-      patches = [
-        ./nix/hide-in-pantheon.patch
-      ];
+                    nativeBuildInputs = [
+                        pkgs.meson
+                        pkgs.ninja
+                        pkgs.pkg-config
+                        pkgs.vala
+                        pkgs.glib
+                        pkgs.bamf
+                        pkgs.wrapGAppsHook  # Added for GSettings support
+                    ];
 
-      meta = with pkgs.lib; {
-        description = "A simple dock for X11 environments";
-        license = licenses.mit;
-        platforms = platforms.linux;
-      };
+                    buildInputs = [
+                        pkgs.gnome-settings-daemon
+                        pkgs.dconf
+                        pkgs.glib
+                        pkgs.git
+                        pkgs.gtk3
+                        pkgs.gnome-menus
+                        pkgs.libgee
+                        pkgs.libwnck
+                        pkgs.pango
+                        pkgs.desktop-file-utils
+                    ];
+
+                    # Compile schemas in post-install phase
+                    postInstall = ''
+                        glib-compile-schemas $out/share/glib-2.0/schemas
+                    '';
+
+                    patches = [
+                        ./nix/hide-in-pantheon.patch
+                    ];
+
+                    meta = with pkgs.lib; {
+                        description = "A simple dock for X11 environments";
+                        license = licenses.mit;
+                        platforms = platforms.linux;
+                    };
+                };
+                default = self.packages.${system}.plank-reloaded;
+            });
+        defaultPackage = forAllSystems (system: self.packages.${system}.plank-reloaded);
     };
-  };
 }
